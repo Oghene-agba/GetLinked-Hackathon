336ea406744030aca57e66265637181a278afb7f
Oghene Agba <72680556+Oghene-agba@users.noreply.github.com>
2023-09-23 23:27:54 +0100

    Update register.js

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,48 +1,130 @@
 const formEl = document.getElementById("registerForm");
 const submitButton = document.getElementById("submitForm");
-const resultMessage = document.getElementById("resultMessage");
+const errorContainer = document.getElementById("errorContainer");
+const agreedCheckbox = document.getElementById("privacy_policy");
 
-submitButton.addEventListener("click", async (event) => {
-  event.preventDefault(); // Prevent the default form submission
-
-  const formData = new FormData(formEl);
-  const data = Object.fromEntries(formData);
-  console.log(data);
-
-  // Check if any of the form fields is empty
-  if (Object.values(data).some((value) => !value)) {
-    resultMessage.textContent = "Please fill out all fields.";
-    return; // Exit the function to prevent sending an empty form
-  }
-
+const fetchCategories = async () => {
   try {
-    const baseUrl = "https://backend.getlinked.ai"; // Replace with your actual base URL
-    const apiUrl = `${baseUrl}/hackathon/register`;
+    const baseUrl = "https://backend.getlinked.ai";
+    const apiUrl = `${baseUrl}/hackathon/categories-list`;
+    const res = await fetch(apiUrl);
+    const data = await res.json();
+    if (data) {
+      return data;
+    }
+  } catch (error) {
+    console.error(error);
 
-    const response = await fetch(apiUrl, {
+    throw error;
+  }
+};
+
+fetchCategories()
+  .then((categoriesData) => {
+    const errorContainer = document.getElementById("errorContainer");
+    const categorySelect = document.getElementById("category");
+
+    categoriesData.forEach((category) => {
+      const option = document.createElement("option");
+      option.value = category.id;
+      option.textContent = category.name;
+      categorySelect.appendChild(option);
+    });
+  })
+  .catch((error) => {
+    console.error("Error fetching categories:", error);
+    errorContainer.textContent = "Error Feching the categories";
+  });
+
+function resetInputFields() {
+  const teamNameInput = document.getElementById("teamName");
+  const phoneInput = document.getElementById("phone");
+  const emailInput = document.getElementById("email");
+  const projectTopicInput = document.getElementById("projectTopic");
+  const categorySelect = document.getElementById("category");
+  const groupSizeSelect = document.getElementById("groupSize");
+
+  teamNameInput.value = "";
+  phoneInput.value = "";
+  emailInput.value = "";
+  projectTopicInput.value = "";
+  categorySelect.value = "";
+  groupSizeSelect.value = "";
+  agreedCheckbox.checked = false;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const submitFormButton = document.getElementById("submitForm");
+  const errorContainer = document.getElementById("errorContainer");
+  const agreedCheckbox = document.getElementById("privacy_policy");
+  const successModal = document.getElementById("successModal");
+  const backBtn = document.getElementById("back");
+
+  submitFormButton.addEventListener("click", function () {
+    const teamNameInput = document.getElementById("teamName");
+    const phoneInput = document.getElementById("phone");
+    const emailInput = document.getElementById("email");
+    const projectTopicInput = document.getElementById("projectTopic");
+    const categorySelect = document.getElementById("category");
+    const groupSizeSelect = document.getElementById("groupSize");
+
+    submitFormButton.textContent = "Processing...";
+
+    if (
+      teamNameInput.value === "" ||
+      phoneInput.value === "" ||
+      emailInput.value === "" ||
+      projectTopicInput.value === "" ||
+      categorySelect.value === "" ||
+      groupSizeSelect.value === "" ||
+      !agreedCheckbox.checked
+    ) {
+      errorContainer.textContent = "Error: Please fill in all required fields.";
+      return;
+    }
+
+    errorContainer.textContent = "";
+
+    const formData = {
+      team_name: teamNameInput.value,
+      phone_number: phoneInput.value,
+      email: emailInput.value,
+      project_topic: projectTopicInput.value,
+      category: +categorySelect.value,
+      group_size: +groupSizeSelect.value,
+      privacy_poclicy_accepted: agreedCheckbox.checked,
+    };
+
+    const baseUrl = "https://backend.getlinked.ai";
+    const apiUrl = `${baseUrl}/hackathon/registration`;
+
+    fetch(apiUrl, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(data),
-    });
+      body: JSON.stringify(formData),
+    })
+      .then((data) => {
+        if (data.status === 201) {
+          successModal.classList.remove("hidden");
+          submitFormButton.textContent = "Register Now";
+          successModal.classList.add("flex");
+          backBtn.addEventListener("click", () => {
+            successModal.classList.add("hidden");
+            resetInputFields();
+          });
+        } else if (data.status === 400) {
+          submitFormButton.textContent = "Register Now";
+          errorContainer.textContent = "Error: Registration failed.";
+        }
+      })
+      .catch((error) => {
+        successModal.classList.add("hidden");
 
-    const responseData = await response.json();
-
-    // Display a success message or handle errors
-    if (responseData) {
-      resultMessage.textContent = `${responseData.first_name} your request has been recieved we will get in touch with you soon`;
-      console.log(responseData);
-    } else {
-      resultMessage.textContent = "An error occurred/invalid data";
-    }
-
-    // Clear the message after a delay (e.g., 5 seconds)
-    setTimeout(() => {
-      resultMessage.textContent = "";
-    }, 5000); // 5000 milliseconds (5 seconds)
-  } catch (error) {
-    console.error("Error:", error);
-    resultMessage.textContent = "An error occurred.";
-  }
+        console.error("Fetch Error:", error);
+        errorContainer.textContent =
+          "Error: An unexpected error occurred while sending the form data. Check internet and try agian";
+      });
+  });
 });
